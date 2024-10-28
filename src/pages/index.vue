e9b2cdae355792ab8f9dc46117a1d488159c6a38
Onaje Oladuwa <onaje.oladuwa@groundfloor.us>
2024-10-28 11:31:18 -0400

    feat: add initial reactivity. data shows in app

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,13 +1,4 @@
-<script setup lang="ts">
-import { supabase } from '@/lib/supabaseClient'
-;(async () => {
-  const { data, error } = await supabase.from('projects').select()
-  if (error) {
-    console.error(error)
-  }
-  console.log('projects', data)
-})()
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <div>
--- src/pages/projects/index.vue
+++ src/pages/projects/index.vue
@@ -1,8 +1,23 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { supabase } from '@/lib/supabaseClient'
+import { ref } from 'vue'
+
+const projects = ref()
+;(async () => {
+  const { data, error } = await supabase.from('projects').select()
+  if (error) {
+    console.error(error)
+  }
+
+  projects.value = data
+  console.log('projects', projects.value)
+})()
+</script>
 
 <template>
   <div>
     <h1>Projects Page</h1>
     <RouterLink to="/">Go Home</RouterLink>
+    {{ projects }}
   </div>
 </template>
