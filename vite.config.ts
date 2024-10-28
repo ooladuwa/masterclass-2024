7596bcce478091dbf3d1ea4bd654c523cbc7a61b
Onaje Oladuwa <onaje.oladuwa@groundfloor.us>
2024-10-28 12:30:53 -0400

    feat: add shadcn-vue

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,17 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import VueRouter from 'unplugin-vue-router/vite'
+import autoprefixer from 'autoprefixer'
+import tailwind from 'tailwindcss'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [VueRouter(), vue()],
+  css: {
+    postcss: {
+      plugins: [tailwind(), autoprefixer()],
+    },
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
