7edc8d4fc6689dcf04880d483358643d1f273e1b
Onaje Oladuwa <onaje.oladuwa@groundfloor.us>
2024-10-28 11:10:17 -0400

    feat: add new seed.js from tutorial repo

--- database/seed.js
+++ database/seed.js
@@ -6,7 +6,19 @@ const supabase = createClient(
   process.env.SERVICE_ROLE_KEY,
 )
 
+const logErrorAndExit = (tableName, error) => {
+  console.error(
+    `An error occurred in table '${tableName}' with code ${error.code}: ${error.message}`,
+  )
+  process.exit(1)
+}
+
+const logStep = stepMessage => {
+  console.log(stepMessage)
+}
+
 const seedProjects = async numEntries => {
+  logStep('Seeding projects...')
   const projects = []
 
   for (let i = 0; i < numEntries; i++) {
@@ -20,7 +32,22 @@ const seedProjects = async numEntries => {
     })
   }
 
-  await supabase.from('projects').insert(projects)
+  const { data, error } = await supabase
+    .from('projects')
+    .insert(projects)
+    .select('id')
+
+  if (error) return logErrorAndExit('Projects', error)
+
+  logStep('Projects seeded successfully.')
+
+  return data
 }
 
-await seedProjects(10)
+const seedDatabase = async numEntriesPerTable => {
+  await seedProjects(numEntriesPerTable)
+}
+
+const numEntriesPerTable = 10
+
+seedDatabase(numEntriesPerTable)
