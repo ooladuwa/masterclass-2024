7bc9ed9d6c4fe2bf6d77b0ad4211d47fdded082a
Onaje Oladuwa <onaje.oladuwa@groundfloor.us>
2024-10-29 10:55:53 -0400

    add suspense component. add dynamic page titles. create pinia store.

--- vite.config.ts
+++ vite.config.ts
@@ -33,9 +33,13 @@ export default defineConfig({
         // presets
         'vue',
         VueRouterAutoImports,
+        {
+          pinia: ['defineStore', 'storeToRefs', 'acceptHMRUpdate'],
+        },
       ],
       dts: true,
       viteOptimizeDeps: true,
+      dirs: ['src/stores'],
     }),
     Components({
       /* options */
