c098e0b9da260f5916ab123f865806ad0324e343
Onaje Oladuwa <onaje.oladuwa@groundfloor.us>
2024-10-29 11:17:37 -0400

    feat: retrieve project details for tasks

--- src/pages/tasks/index.vue
+++ src/pages/tasks/index.vue
@@ -3,22 +3,25 @@ import { supabase } from '@/lib/supabaseClient'
 import type { Tables } from '../../../database/types'
 import type { ColumnDef } from '@tanstack/vue-table'
 import { RouterLink } from 'vue-router'
+import type { QueryData } from '@supabase/supabase-js'
 
 usePageStore().pageData.title = 'Tasks'
 
-const tasks = ref<Tables<'tasks'>[] | null>(null)
+const tasksWithProjectsQuery = supabase.from('tasks').select(`
+*,
+projects (
+  id,
+  name,
+  slug
+)
+`)
+
+type TasksWithProjects = QueryData<typeof tasksWithProjectsQuery>
+
+const tasks = ref<TasksWithProjects | null>(null)
 const getTasks = async () => {
-  const { data, error } = await supabase.from('tasks').select(`
-  *,
-  projects (
-    id,
-    name,
-    slug
-  )
+  const { data, error } = await tasksWithProjectsQuery
 
-  `)
-
-  console.log(data)
   if (error) {
     console.error(error)
   }
@@ -27,7 +30,7 @@ const getTasks = async () => {
 
 await getTasks()
 
-const columns: ColumnDef<Tables<'tasks'>>[] = [
+const columns: ColumnDef<TasksWithProjects[0]>[] = [
   {
     accessorKey: 'name',
     header: () => h('div', { class: 'text-left' }, 'Name'),
@@ -68,14 +71,16 @@ const columns: ColumnDef<Tables<'tasks'>>[] = [
     accessorKey: 'projects',
     header: () => h('div', { class: 'text-left' }, 'Project'),
     cell: ({ row }) => {
-      return h(
-        RouterLink,
-        {
-          to: `/projects/${row.original.projects.slug}`,
-          class: 'text-left font-medium hover:bg-muted block w-full',
-        },
-        () => row.getValue('projects').name,
-      )
+      return row.original.projects
+        ? h(
+            RouterLink,
+            {
+              to: `/projects/${row.original.projects.slug}`,
+              class: 'text-left font-medium hover:bg-muted block w-full',
+            },
+            () => row.original.projects?.name,
+          )
+        : ''
     },
   },
   {
